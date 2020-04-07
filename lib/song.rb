f8110278abf44b0dfa35aedf647d6cd18b192c43
Dametrik Fick <dametrik.fick@gmail.com>
2020-04-07 06:33:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,15 @@ require 'pry'
 
 class Song
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Paramable::InstanceMethods
-
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
 
   def self.all
     @@songs
@@ -21,6 +18,6 @@ class Song
 
   def artist=(artist)
     @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
   end
-
 end
